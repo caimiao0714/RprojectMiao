5aabe317bd64dea7f76ed1b759286fb2e1067731
Miao Cai <miaocai@slu.edu>
2020-05-10 19:04:32 -0500

    add code for MO COVID-19

--- misce/MO-COVID-19/missouri_covid19.R
+++ misce/MO-COVID-19/missouri_covid19.R
@@ -1,42 +1,41 @@
-##Read in data
-total1 <- read.csv("D:/×ÀÃæ/disease mapping/dailyupdate.csv")
-#MO_map <- read.csv("D:/×ÀÃæ/disease mapping/MO_2018_County_Boundaries.csv")
-head(MO_map)
-shape <- st_read(
-  "D:/×ÀÃæ/disease mapping/geo_export_714e58d7-558f-4df1-af96-16ca5d24323f.shp")
-library(sf)
-library( tmap )
-library( rgdal )
-library("data.table")
-library(tidyverse)
-library( tigris )
-library(dplyr)
-library(plyr)
-library(reshape)
-library(naniar)
-###read in COVID DATA and prepare covid data for merge,make sure the variables name are the same.
-###shape file doesn't have kansas city, so I have to delete cansas city in covid data.
-MO_covid<- subset(total1,state=='Missouri'& ï»¿date=='2020/4/15')
-MO_covid <- rename(MO_covid,c(county="countyname",fips="countyfips"))
-MO_covid_noKan <- subset(MO_covid, countyname!="Kansas City"&countyname!="Unknown")
-###SHAPE FILE
-#county name are not consistant between two data.
-shape$countyname <- revalue(shape$countyname, 
-                            c("St Charles"="St. Charles","St Clair"="St. Clair",
-                              "St Francois"="St. Francois",
-                              "St Louis"="St. Louis","St Louis City"="St. Louis city",
-                              "Ste Genevieve"="Ste. Genevieve","Dekalb"="DeKalb"
-                              ))
-###MERGE 
-merge_MO<- merge(x = shape, y = MO_covid_noKan, by = "countyname", all = TRUE)
-merge415 <- subset(merge_MO,ï»¿date=='2020/4/15')
-merge_MO$cases[is.na(merge_MO$cases)] <- 0
-max(merge_MO$cases)
-min(merge_MO$cases)
-##TIME SERIES
-MO_covid_415 <- subset(merge_MO, ï»¿date=='2020/4/15')
-MO_covid_508 <- subset(merge_MO, ï»¿date=='2020/5/8')
-###MAP
-windows(9,7)
-tm_shape(MO_covid_508) +
-  tm_polygons("cases",style="fixed" , breaks=c(0,10,100,500,1000,2000,3700))
+pacman::p_load(dplyr, data.table, sf, tigris, lubridate, tmap)
+# read and filter MO COVID-19 data
+d = fread("misce/MO-COVID-19/dailyupdate.csv") %>%
+  .[state == 'Missouri',] %>%
+  .[,date := ymd(date)] %>%
+  setkey(date, county)
+
+# get MO county boundaries for MO
+MO = tigris::counties(state = 'MO', year = 2018) %>%
+  st_as_sf() %>%
+  mutate(FPS_code = as.numeric(paste0(STATEFP, COUNTYFP))) %>%
+  select(FPS_code, county_name = NAME)
+
+# test merging
+mapMO = function(a_date){
+  p = MO %>%
+    left_join(d[date == ymd(a_date),], by = c('FPS_code' = 'fips')) %>%
+    mutate(cases = if_else(is.na(cases), 0L, cases)) %>%
+    tm_shape() +
+    tm_polygons("cases", style="fixed",
+                breaks=c(0,10,100,500,1000,2000,3700)) +
+    tm_layout(main.title = paste0("Number of confirmed COVID-19 in Missouri, ",
+                             format(ymd(a_date), '%b %d, %Y')),
+              main.title.position = "center")
+  return(p)
+}
+
+mapMO('2020-5-6')
+mapMO('2020-5-7')
+mapMO('2020-5-8')
+
+
+
+
+
+
+
+
+
+
+
