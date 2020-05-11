ef8ea0ada8b4c3bf8af28b6068b8b41db9d08ff4
Miao Cai <miaocai@slu.edu>
2020-05-10 21:17:11 -0500

    add model performance code from performance package in R

--- misce/IQ/mgcv_code.R
+++ misce/IQ/mgcv_code.R
@@ -1,12 +1,12 @@
-pacman::p_load(dplyr, data.table, lme4, mgcv, broom)
+pacman::p_load(dplyr, data.table, lme4, mgcv, broom, performance)
 
 d = fread('misce/IQ/Lead_Final(1).csv') %>%
   .[,`:=`(ses = factor(ses),
           school = factor(school))]
 
-f0 = glm(IQ ~ Pb + age + sex + BMI + factor(ses) + interest + ownhome,
+f0 = glm(IQ ~ Pb + age + sex + BMI + ses + interest + ownhome,
          data = d)
-f1 = lmer(IQ ~ Pb + age + sex + BMI + factor(ses) + interest + ownhome +
+f1 = lmer(IQ ~ Pb + age + sex + BMI + ses + interest + ownhome +
              (1|school),
          data = d)
 
@@ -16,10 +16,12 @@ f2 = mgcv::gam(IQ ~ s(Pb, k = 15, bs = 'tp') + age + sex + BMI + ses + interest 
 f3 = mgcv::gam(IQ ~ s(Pb, k = 15, bs = 'tp') + s(school, bs = 're') + age + sex + BMI + ses + interest + ownhome,
                data = d, method="REML", family="gaussian")
 
-glance(f0)
-glance(f1) # this seems to be the best model
-glance(f2)
-glance(f3)
+model_performance(f0)
+model_performance(f1) # this seems to be the best model
+model_performance(f2)
+model_performance(f3)
+
+icc(f1)
 
 plot(f2)
 plot(f3)
