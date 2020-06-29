81cf9b225df2f1ee327f7af2001c49d068913127
Miao Cai <miaocai@slu.edu>
2020-06-28 19:56:15 -0500

    add real data analysis for GAM

---
+++ GAM/real_data.R
@@ -0,0 +1,25 @@
+pacman::p_load(data.table, dplyr, mgcv, MASS)
+
+d0 = fread('GAM/Wuhan.csv') %>% .[case > 0,.(case, death, PM2.5, t, h, w)]
+d1 = fread('GAM/Beijing.csv') %>% .[,.(case, death, PM2.5, t, h, w)]
+
+
+f0 = gam(death ~ PM2.5 + t + h + w + offset(log(case)),
+         family = nb(link='log'), data = d0, method = 'REML')
+f1 = gam(death ~ s(PM2.5) + t + h + w + offset(log(case)),
+         family = nb(link='log'), data = d0, method = 'REML')
+
+summary(f0)
+summary(f1)
+
+
+f1 = gam(death ~ s(PM2.5) + t + h + w + offset(log(case)),
+         family = nb(link='log'), data = d1, method = 'REML')
+plot(f0)
+plot(f1)
+
+summary(f1)
+
+
+
+
